c52a3f1543d6a4b96e7f427305db6a7a8c384499
antonic901 <antonicnikola6@gmail.com>
2026-02-09 00:17:07 +0100

    added: disable some asserts so programs doesn't crash - this is bad if those exceptions really occure!

--- lib/xboxrt/vcruntime/eh.cpp
+++ lib/xboxrt/vcruntime/eh.cpp
@@ -8,54 +8,54 @@
 
 void __cdecl __ExceptionPtrCreate (void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 void __cdecl __ExceptionPtrDestroy (void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 void __cdecl __ExceptionPtrCopy (void*, const void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 void __cdecl __ExceptionPtrAssign (void*, const void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 bool __cdecl __ExceptionPtrCompare (const void*, const void*)
 {
-    assert(0);
-    return false;
+    // assert(0);
+    return true;
 }
 
 bool __cdecl __ExceptionPtrToBool (const void*)
 {
-    assert(0);
-    return false;
+    // assert(0);
+    return true;
 }
 
 void __cdecl __ExceptionPtrSwap (void*, void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 void __cdecl __ExceptionPtrCurrentException (void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 [[noreturn]] void __cdecl __ExceptionPtrRethrow (const void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 void __cdecl __ExceptionPtrCopyException (void*, const void*, const void*)
 {
-    assert(0);
+    // assert(0);
 }
 
 extern "C"
